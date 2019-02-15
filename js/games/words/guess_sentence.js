f988785e4ee40921b793059a3b081c5780dba2d6
CROR3D <sider3d@gmail.com>
2019-02-15 14:26:30 +0100

    Guess Sentence Cleaning #1

--- js/games/words/guess_sentence.js
+++ js/games/words/guess_sentence.js
@@ -1,43 +1,135 @@
+var gameVariables = {
+    gameData: {
+        title: sessionStorage.getItem('gameLink'),
+        description: 'Find out what sentence is hidden by guessing letters every turn. You start with 5 points. If you guess wrong you will lose 1 point. Getting to 0 points - game ends.',
+        sentenceList: {
+            level1: [
+                {
+                    hint: 'Question',
+                    content: 'Are you going to school today?'
+                },
+                {
+                    hint: 'Question',
+                    content: 'Are you going to school today?'
+                },
+                {
+                    hint: 'Question',
+                    content: 'Are you going to school today?'
+                }
+            ]
+        }
+    },
+    selectUI: {
+        header: document.querySelector('#playHeader'),
+        description: document.querySelector('#descriptionHeader'),
+        statBox: document.querySelector('#statBox'),
+        gameBoard: document.querySelector('#gameBoard'),
+        gameBoardUpper: document.querySelector('#gameBoardUpper'),
+        gameBoardLower: document.querySelector('#gameBoardLower'),
+        modal: document.querySelector('#modal'),
+        modalTitle: document.querySelector('#modal-title')
+    }
+}
+
+const HelperFunctions = (function ()
+{
+    return {
+        getEmpty: function(sentence)
+        {
+            let array = [];
+
+            for (var i = 0; i < sentence.length; i++) {
+                if(sentence[i] == ' ') array.push('\xa0');
+                if(sentence[i] != ' ') array.push('_');
+                if(sentence[i] == '?' || sentence[i] == '!') array.push(sentence[i]);
+            }
+            array = array.join('');
+
+            return array.toString();
+        },
+        setProgressSentence: function(sentence, progress, letter)
+        {
+            let array = [];
+
+            for (var i = 0; i <= sentence.length; i++) {
+                if(sentence[i] == letter)
+                {
+                    array.push(letter);
+                }
+                else
+                {
+                    array.push(progress[i]);
+                }
+            }
+
+            array = array.join('');
+            return array.toString();
+        }
+    }
+})();
+
+var gameSelected = {
+    currentHint: gameVariables.gameData.sentenceList.level1[0].hint,
+    currentSentence: gameVariables.gameData.sentenceList.level1[0].content
+}
+
+var gameState = {
+    isLost: false,
+    highScore: 0,
+    currentLevel: 1,
+    currentScore: 5,
+    sentenceProgress: HelperFunctions.getEmpty(gameSelected.currentSentence)
+}
+
 const GameUI = (function ()
 {
+    function statBoxUI()
+    {
+        const hint = document.createElement('div'),
+              level = document.createElement('div'),
+              score = document.createElement('div');
+
+        hint.classList.add('col', 'mb-3');
+        hint.style.fontSize = 'x-large';
+        hint.innerText = `Hint: ${gameSelected.currentHint}`;
+
+        level.classList.add('col', 'mb-3');
+        level.style.fontSize = 'x-large';
+        level.innerText = `Level: ${gameState.currentLevel}`;
+
+        score.classList.add('col', 'mb-3');
+        score.style.fontSize = 'x-large';
+        score.setAttribute('id', 'score-table');
+        score.innerText = `Score: ${gameState.currentScore}`;
+
+        gameVariables.selectUI.statBox.appendChild(hint);
+        gameVariables.selectUI.statBox.appendChild(level);
+        gameVariables.selectUI.statBox.appendChild(score);
+    }
+
     function gameBoardUpperUI()
     {
-        const playHeader = document.querySelector('#playHeader'),
-              gameBoard = document.querySelector('#gameBoard'),
-              gameBoardUpper = document.querySelector('#gameBoardUpper'),
-              gameTitle = document.createElement('h1'),
-              hint = document.createElement('p');
+        const gameTitle = document.createElement('h1');
 
         gameTitle.classList.add('display-4', 'text-center');
-        gameTitle.innerText = 'Guess Sentence';
+        gameTitle.innerText = gameVariables.gameData.title;
 
-        playHeader.appendChild(gameTitle);
-        gameBoardUpper.style.fontSize = 'x-large';
-        gameBoardUpper.style.letterSpacing = "10px";
-        gameBoardUpper.innerText = 'sentenceProgress';
-
-        hint.classList.add('text-center', 'mb-3');
-        hint.style.fontSize = 'x-large';
-        hint.innerText = 'Hint: ';
-
-        gameBoard.insertBefore(hint, gameBoardUpper);
+        gameVariables.selectUI.header.appendChild(gameTitle);
+        gameVariables.selectUI.gameBoardUpper.style.fontSize = 'x-large';
+        gameVariables.selectUI.gameBoardUpper.style.letterSpacing = "10px";
+        gameVariables.selectUI.gameBoardUpper.innerText = gameState.sentenceProgress;
+        gameVariables.selectUI.description.innerText = gameVariables.gameData.description;
     }
 
     function gameBoardLowerUI()
     {
-        const gameBoardLower = document.querySelector('#gameBoardLower'),
-              score = document.createElement('div'),
-              inputDiv = document.createElement('div'),
+        const inputDiv = document.createElement('div'),
               userInput = document.createElement('input'),
               button = document.createElement('button');
 
-        gameBoardLower.classList.add('row');
+        gameVariables.selectUI.gameBoardLower.classList.add('row');
 
-        gameBoardLower.appendChild(score);
-        gameBoardLower.appendChild(inputDiv);
-
-        score.classList.add('text-left', 'p-3');
-        score.style.fontSize = 'x-large';
+        gameVariables.selectUI.gameBoardLower.appendChild(inputDiv);
 
         inputDiv.classList.add('p-3');
         inputDiv.style.fontSize = 'x-large';
@@ -52,8 +144,6 @@ const GameUI = (function ()
         button.setAttribute('id', 'try-btn');
         button.innerText = 'Try';
 
-        score.setAttribute('id', 'score-table');
-        score.innerText = 'Score: ';
         inputDiv.innerText = 'Letter: ';
         inputDiv.appendChild(userInput);
         inputDiv.appendChild(button);
@@ -62,107 +152,58 @@ const GameUI = (function ()
     return {
         init: function()
         {
+            statBoxUI();
             gameBoardUpperUI();
             gameBoardLowerUI();
         }
     }
 })();
 
-const GuessSentence = (function (gameUI)
+const GuessSentence = (function (gameUI, helperFunctions)
 {
-    const title = sessionStorage.getItem('gameLink'),
-          description = 'Find out what sentence is hidden by guessing letters every turn. You start with 5 points. If you guess wrong you will lose 1 point. Getting to 0 points - game ends.';
-
-    let sentence = {
-        hint: 'Question',
-        content: 'Are you going to school today?'
-    }
-
-    let pickedSentence = sentence.content;
-        totalScore = 5,
-        lettersCount = pickedSentence.length,
-        sentenceProgress = getEmpty(),
-        modal = document.querySelector('#modal'),
-        modalTitle = document.querySelector('#modal-title'),
-        letter = '',
-        isLost = false;
-
-    function getEmpty()
-    {
-        let array = [];
-
-        for (var i = 0; i < lettersCount; i++) {
-            if(pickedSentence[i] == ' ') array.push('\xa0');
-            if(pickedSentence[i] != ' ') array.push('_');
-            if(pickedSentence[i] == '?' || pickedSentence[i] == '!') array.push(pickedSentence[i]);
-        }
-
-        array = array.join('');
-        return array.toString();
-    }
-
-    function setProgressSentence(progress, letter)
-    {
-        let array = [];
-
-        for (var i = 0; i <= lettersCount; i++) {
-            if(pickedSentence[i] == letter)
-            {
-                array.push(letter);
-            }
-            else
-            {
-                array.push(progress[i]);
-            }
-        }
-
-        array = array.join('');
-        sentenceProgress = array.toString();
-    }
+    let letter = '';
 
     function executeGame(sentence)
     {
         const tryBtn = document.getElementById('try-btn'),
               userInput = document.getElementById('user-input'),
-              gameBoardUpper = document.querySelector('#gameBoardUpper'),
               score = document.getElementById('score-table');
 
         tryBtn.addEventListener('click', letterAttempt);
 
         function letterAttempt()
         {
-            if(isLost) window.location.reload();
+            if(gameState.isLost) window.location.reload();
 
             letter = userInput.value;
 
-            const alert = document.createElement('div'),
-                  gameBoard = document.querySelector('#gameBoard');
+            const alert = document.createElement('div');
 
                   if(document.querySelector('.alert'))
                   {
-                      gameBoard.removeChild(document.querySelector('.alert'));
+                      gameVariables.selectUI.gameBoard.removeChild(document.querySelector('.alert'));
                   }
 
             alert.classList.add('alert', 'alert-danger', 'text-center');
             alert.setAttribute('role', 'alert');
             alert.innerText = `NO LETTER ${letter.toUpperCase()}`;
 
-            if(pickedSentence.includes(letter))
+            if(gameSelected.currentSentence.includes(letter))
             {
-                setProgressSentence(sentenceProgress, letter);
-                gameBoardUpper.innerText = sentenceProgress;
+                gameState.sentenceProgress = helperFunctions.setProgressSentence(gameSelected.currentSentence, gameState.sentenceProgress, letter);
+                gameVariables.selectUI.gameBoardUpper.innerText = gameState.sentenceProgress;
             }
             else
             {
-                gameBoard.insertBefore(alert, gameBoardUpper);
-                totalScore--;
-                if(totalScore == 0)
+                gameVariables.selectUI.gameBoard.insertBefore(alert, gameBoardUpper);
+                gameState.currentScore--;
+                if(gameState.currentScore == 0)
                 {
-                    isLost = true;
-                    modal.style.display = 'block';
-                    modalTitle.innerText = 'GAME OVER';
+                    gameState.isLost = true;
+                    gameVariables.selectUI.modal.style.display = 'block';
+                    gameVariables.selectUI.modalTitle.innerText = 'GAME OVER';
                 }
-                score.innerText = `Score: ${totalScore}`;
+                score.innerText = `Score: ${gameState.currentScore}`;
             }
         }
     }
@@ -171,10 +212,10 @@ const GuessSentence = (function (gameUI)
         init: function()
         {
             gameUI.init();
-            executeGame(pickedSentence);
+            executeGame(gameState.currentSentence);
         }
     }
 
-})(GameUI);
+})(GameUI, HelperFunctions);
 
 GuessSentence.init();
