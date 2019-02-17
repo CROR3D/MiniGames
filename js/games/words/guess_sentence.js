f651a3075f48ebfd0b1c30aef2ff870f00b75a5d
CROR3D <sider3d@gmail.com>
2019-02-17 20:24:25 +0100

    Guess Sentence Cleaning #2

--- js/games/words/guess_sentence.js
+++ js/games/words/guess_sentence.js
@@ -6,7 +6,7 @@ var gameVariables = {
             level1: [
                 {
                     hint: 'Question',
-                    content: 'Are you going to school today?'
+                    content: 'Hello'
                 },
                 {
                     hint: 'Question',
@@ -64,6 +64,15 @@ const HelperFunctions = (function ()
 
             array = array.join('');
             return array.toString();
+        },
+        getGuessedCount: function(sentence, progress, letter)
+        {
+            let count = 0;
+
+            for (var i = 0; i <= sentence.length; i++) {
+                if(sentence[i] == letter) count++;
+            }
+            return count;
         }
     }
 })();
@@ -77,34 +86,37 @@ var gameState = {
     isLost: false,
     highScore: 0,
     currentLevel: 1,
-    currentScore: 5,
-    sentenceProgress: HelperFunctions.getEmpty(gameSelected.currentSentence)
+    currentScore: 5, // TODO change to attemptsLeft
+    sentenceProgress: HelperFunctions.getEmpty(gameSelected.currentSentence),
+    lettersLeft: gameSelected.currentSentence.replace(/[^a-zA-Z]/g, "").length
 }
 
 const GameUI = (function ()
 {
     function statBoxUI()
     {
-        const hint = document.createElement('div'),
+        const attempt = document.createElement('div'),
               level = document.createElement('div'),
               score = document.createElement('div');
-
-        hint.classList.add('col', 'mb-3');
-        hint.style.fontSize = 'x-large';
-        hint.innerText = `Hint: ${gameSelected.currentHint}`;
-
-        level.classList.add('col', 'mb-3');
-        level.style.fontSize = 'x-large';
-        level.innerText = `Level: ${gameState.currentLevel}`;
 
         score.classList.add('col', 'mb-3');
         score.style.fontSize = 'x-large';
         score.setAttribute('id', 'score-table');
-        score.innerText = `Score: ${gameState.currentScore}`;
+        score.innerText = `Score: ${gameState.highScore}`;
 
-        gameVariables.selectUI.statBox.appendChild(hint);
-        gameVariables.selectUI.statBox.appendChild(level);
+        level.classList.add('col', 'mb-3');
+        level.style.fontSize = 'x-large';
+        level.setAttribute('id', 'level-table');
+        level.innerText = `Level: ${gameState.currentLevel}`;
+
+        attempt.classList.add('col', 'mb-3');
+        attempt.style.fontSize = 'x-large';
+        attempt.setAttribute('id', 'attempt-table');
+        attempt.innerText = `Attempts: ${gameState.currentScore}`;
+
         gameVariables.selectUI.statBox.appendChild(score);
+        gameVariables.selectUI.statBox.appendChild(level);
+        gameVariables.selectUI.statBox.appendChild(attempt);
     }
 
     function gameBoardUpperUI()
@@ -140,7 +152,7 @@ const GameUI = (function ()
         userInput.setAttribute('size', '1');
         userInput.setAttribute('maxlength', '1');
 
-        button.classList.add('btn', 'btn-secondary', 'ml-2', 'mb-2');
+        button.classList.add('btn', 'btn-success', 'ml-2', 'mb-2');
         button.setAttribute('id', 'try-btn');
         button.innerText = 'Try';
 
@@ -155,6 +167,16 @@ const GameUI = (function ()
             statBoxUI();
             gameBoardUpperUI();
             gameBoardLowerUI();
+        },
+        update: function(score, level, attempts)
+        {
+            const getScoreTable = document.getElementById('score-table'),
+                  getLevelTable = document.getElementById('level-table'),
+                  getAttemptTable = document.getElementById('attempt-table');
+
+            getScoreTable.innerText = `Score: ${score}`;
+            getLevelTable.innerText = `Level: ${level}`;
+            getAttemptTable.innerText = `Attempts: ${attempts}`;
         }
     }
 })();
@@ -163,11 +185,11 @@ const GuessSentence = (function (gameUI, helperFunctions)
 {
     let letter = '';
 
-    function executeGame(sentence)
+    function executeGame()
     {
         const tryBtn = document.getElementById('try-btn'),
               userInput = document.getElementById('user-input'),
-              score = document.getElementById('score-table');
+              attempt = document.getElementById('attempt-table');
 
         tryBtn.addEventListener('click', letterAttempt);
 
@@ -179,10 +201,10 @@ const GuessSentence = (function (gameUI, helperFunctions)
 
             const alert = document.createElement('div');
 
-                  if(document.querySelector('.alert'))
-                  {
-                      gameVariables.selectUI.gameBoard.removeChild(document.querySelector('.alert'));
-                  }
+            if(document.querySelector('.alert'))
+            {
+              gameVariables.selectUI.gameBoard.removeChild(document.querySelector('.alert'));
+            }
 
             alert.classList.add('alert', 'alert-danger', 'text-center');
             alert.setAttribute('role', 'alert');
@@ -191,6 +213,8 @@ const GuessSentence = (function (gameUI, helperFunctions)
             if(gameSelected.currentSentence.includes(letter))
             {
                 gameState.sentenceProgress = helperFunctions.setProgressSentence(gameSelected.currentSentence, gameState.sentenceProgress, letter);
+                let number = helperFunctions.getGuessedCount(gameSelected.currentSentence, gameState.sentenceProgress, letter);
+                gameState.lettersLeft -= number;
                 gameVariables.selectUI.gameBoardUpper.innerText = gameState.sentenceProgress;
             }
             else
@@ -203,16 +227,27 @@ const GuessSentence = (function (gameUI, helperFunctions)
                     gameVariables.selectUI.modal.style.display = 'block';
                     gameVariables.selectUI.modalTitle.innerText = 'GAME OVER';
                 }
-                score.innerText = `Score: ${gameState.currentScore}`;
+                attempt.innerText = `Score: ${gameState.currentScore}`;
             }
+
+            if(gameState.lettersLeft == 0) newLevel();
         }
+    }
+
+    function newLevel()
+    {
+        gameState.highScore += gameState.currentScore;
+        gameState.currentScore = 5;
+        gameState.currentLevel++;
+
+        GameUI.update(gameState.highScore, gameState.currentLevel, gameState.currentScore);
     }
 
     return {
         init: function()
         {
             gameUI.init();
-            executeGame(gameState.currentSentence);
+            executeGame();
         }
     }
 
